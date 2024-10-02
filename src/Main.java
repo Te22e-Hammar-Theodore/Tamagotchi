b0c0b25a3a1cface6b4d6233135ec23514449ad4
Theodore Hammar <theodore.hammar@elev.ga.ntig.se>
2024-10-02 11:21:49 +0200

    Added code to my tamagotchi program

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        Tamagotchi tama = new Tamagotchi("");
+        System.out.println("Välkommen till din Tamagotchi");
+        System.out.println("Vad ska din Tamagotchi heta?");
+        System.out.println();
+        System.out.println("Vad heter din karaktär?");
+        System.out.print(">>> ");
+        tama.name = sc.nextLine();
+
+        System.out.println();
+        System.out.println("Vad vill du göra?");
+        System.out.println("1. Mata " + tama.name);
+        System.out.println("2. Prata med " + tama.name);
+        System.out.println("3. Lära " + tama.name + " ett nytt ord");
+        System.out.println("4. Visa " + tama.name + "s stats");
+        int menuChoise = sc.nextInt();
+        sc.nextLine();
+
+        switch (menuChoise) {
+            case 1:
+                tama.feed();
+                break;
+            case 2:
+tama.speak();
+                break;
+            case 3:
+tama.teach();
+                break;
+            case 4:
+tama.printStats();
+                break;
+            default:
+                System.out.println("Ogiltigt val, försök igen.");
+                continue;
+        }
     }
 }
--- src/Tamagotchi.java
+++ src/Tamagotchi.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
-
+import java.util.*;
 public class Tamagotchi {
+    //Datatypes
+    public String name;
+    private int hunger = 0;
+    private int bordedom = 0;
+    private boolean isAlive;
+    private ArrayList<String> words = new ArrayList<>();
+    private Random generator;
+
+public Tamagotchi(String incomingName) {
+    this.name = incomingName;
+}
+    public void feed() {
+        hunger --;
+    }
+
+    public void speak() {
+
+    bordedom--;
+    }
+
+    public void teach(String word) {
+    words.add(word);
+    }
+
+    public void tick() {
+    bordedom++;
+    hunger ++;
+    }
+
+    public void printStats() {
+        System.out.println(name);
+    System.out.println(hunger);
+        System.out.println(bordedom);
+    }
+
+    public boolean getAlive() {
+        return isAlive;
+    }
+
+    private void reduceBordedom() {}
 }
